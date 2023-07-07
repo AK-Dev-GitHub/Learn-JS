1a02fc9c4b1119ff470d239d64349e4996948050
Akihiro Kaneda <101224342+KanedaAkihiro@users.noreply.github.com>
2023-07-08 00:41:39 +0900

    Fix: Udemyフォルダにセクションフォルダを追加 ・演算子フォルダと変数フォルダをセクションフォルダに移動

---
+++ Sample Code/UdemyJs/セクション1/演算子/main.js
@@ -0,0 +1,15 @@
+// 基本的な算術演算子
+let enzan = 2 + 5;
+console.log(enzan);
+
+// 割り算
+let warizan = 4 / 2;
+console.log(warizan);
+
+// あまりの計算
+let amari = 32 % 5;
+console.log(amari);
+
+// 累乗（例2の3乗）
+let ruiJyou = 2 ** 3;
+console.log(ruiJyou);
