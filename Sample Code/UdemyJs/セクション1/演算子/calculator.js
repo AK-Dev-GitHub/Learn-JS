e5d132bb18876a830539f577be6e2462bdf23ea0
Akihiro Kaneda <101224342+KanedaAkihiro@users.noreply.github.com>
2023-07-09 13:51:53 +0900

    ・Fix: 演算子フォルダ名変更

--- Sample Code/UdemyJs/セクション1/演算子/calculator.js
+++ Sample Code/UdemyJs/セクション1/演算子/calculator.js
@@ -13,3 +13,13 @@ console.log(amari);
 // 累乗（例2の3乗）
 let ruiJyou = 2 ** 3;
 console.log(ruiJyou);
+
+// 省略記法
+let result = 1;
+result = result + 1;
+// 足し算の省略記法一覧
+result += 1;
+// ++が後ろにつくとプラスされる前の値を返却する
+result++;
+// ++が後ろにつくとプラスされる後の値を返却する
+++result;
