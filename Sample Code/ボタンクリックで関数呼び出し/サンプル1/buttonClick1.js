1f7f36c48d210a18e1a276b4ac751672fa462cf3
Akihiro Kaneda <101224342+KanedaAkihiro@users.noreply.github.com>
2023-08-30 01:01:56 +0900

    Add: ボタンクリック関数のサンプル実装 TODO: アラートのOKボタンをクリック後に別の関数が呼び出される処理を追加する

---
+++ Sample Code/ボタンクリックで関数呼び出し/サンプル1/buttonClick1.js
@@ -0,0 +1,10 @@
+function alertButton() {
+  const message = 'OKボタンを押してください';
+  alert(message);
+}
+
+// TODO: アラートのOKボタンをクリック後に別の関数が呼び出される処理を実装する
+function alertButton2() {
+  const movePageMessage = '画面遷移に成功しました';
+  alert(movePageMessage);
+}
