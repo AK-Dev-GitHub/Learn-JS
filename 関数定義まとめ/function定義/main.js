f4cc9cfc679b235ea5e123b4afecdb7d9d1f3fc6
shibu <akihirokaneda@outlook.com>
2024-04-02 00:57:08 +0900

    Add : 関数定義

---
+++ 関数定義まとめ/function定義/main.js
@@ -0,0 +1,16 @@
+//関数の宣言を行うにはfunction命令を使用します。
+// 関数名には処理の説明的な名前をつけるのが一般的です。
+function greet() {
+  console.log('こんにちは!');
+}
+
+greet();
+
+// オリジナル練習関数
+let fruitName =
+  [`apple`, `banana`, `orange`] + 'が買い物かごに入っている果物です。';
+function fruitNameOutput() {
+  console.log(fruitName);
+}
+
+fruitNameOutput();
---
+++ 関数定義まとめ/引数があるfunciton関数/main.js
@@ -0,0 +1,24 @@
+function greet(name) {
+  console.log(`こんにちは${name}さん!`);
+}
+
+// ブラウザのダイアログで名前を入力→name変数に代入
+let name = prompt('あなたの名前は何ですか？');
+
+//関数の呼び出し
+greet(name);
+
+// オリジナル関数
+function personalInfo(pInfo) {
+  console.table(`${pInfo}この情報で会員登録しますか？`);
+}
+
+// ブラウザのダイアログで名前を入力→name変数に代入
+let pInfo = {
+  name: prompt('名前を入力してください'),
+  password: prompt('パスワードを入力してください'),
+  tel: prompt('電話番号を入力してください'),
+};
+
+//関数の呼び出し
+personalInfo(pInfo);
