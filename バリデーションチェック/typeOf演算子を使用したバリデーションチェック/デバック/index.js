0a8feefb261f76fd84897bb1a7e1e01769c7c52c
shibu <akihirokaneda@outlook.com>
2024-04-10 01:46:52 +0900

    Add : typeOf演算子入力の値の検証コード追加

---
+++ バリデーションチェック/typeOf演算子を使用したバリデーションチェック/デバック/index.js
@@ -0,0 +1,29 @@
+// 文字列型
+console.log(typeof 'こんにちは');
+
+// 数値型
+console.log(typeof 10);
+
+// 真偽値型
+console.log(typeof true);
+
+// null型
+console.log(typeof null);
+
+// undefined型
+console.log(typeof hello);
+
+// symbol型
+console.log(typeof Symbol('A'));
+
+// 配列型
+console.log(typeof [1, 2, 3]);
+
+// オブジェクト型
+console.log(typeof { name: '山田' });
+
+// 関数型
+console.log(typeof function () {});
+
+let a = 1;
+console.log(typeof a);
