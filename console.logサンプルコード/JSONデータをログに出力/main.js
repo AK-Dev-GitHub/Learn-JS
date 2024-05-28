dded75cceabb7ecfd1ec9c16979f15614cd1c29f
shibu <akihirokaneda@outlook.com>
2024-05-28 23:39:06 +0900

    Add : JSON形式ログとconsole.gropとの併用

--- console.logサンプルコード/JSONデータをログに出力/main.js
+++ console.logサンプルコード/JSONデータをログに出力/main.js
@@ -6,3 +6,15 @@ const testObj = {
 
 // Nullとインデント2を使用することによって可読性を向上した状態でログを出力可能
 console.log(JSON.stringify(testObj, null, 2));
+
+// console.groupとの併用
+const testObj2 = {
+  name: 'test2',
+  age: 25,
+  hobbies: ['baseball', 'walking', 'music'],
+};
+
+// testObj2のログを出力するグループを開始
+console.group('testObj2');
+console.log(JSON.stringify(testObj2, null, 2));
+console.groupEnd(); // testObj2のログを出力するグループを終了
